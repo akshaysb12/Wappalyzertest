cd23a8850fe65dae3a70bb55edb06f7a0d88941c
akshaysb12 <akshaysb72@gmail.com>
2023-07-31 12:27:26 +0530

    wappalyzercommit

---
+++ src/wappalyzer/wappalyzertest1.java
@@ -0,0 +1,35 @@
+package wappalyzer;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class wappalyzertest1 {
+
+		WebDriver driver;
+		
+		@BeforeTest
+		public void setup()
+		{
+			driver = new ChromeDriver();
+			driver.get("https://www.wappalyzer.com/");
+		}
+		
+		@Test
+		public void test() throws InterruptedException
+		{
+			Thread.sleep(3000);
+			driver.findElement(By.xpath("//*[@id=\"app\"]/div/main/div/div[1]/header/div/div/div/div[2]/button[2]/span")).click();
+			driver.findElement(By.xpath("//*[@id=\"list-item-50\"]")).click();
+			driver.findElement(By.xpath("//*[@id=\"app\"]/div/main/div/div[2]/div[2]/div/div/div[2]/div[1]/div[1]/a/div/div/div[2]/span")).click();
+			driver.findElement(By.xpath("//*[@id=\"app\"]/div/main/div/div[2]/div[1]/div[2]/div/div[3]/div/div/a")).click();
+			driver.findElement(By.xpath("//*[@id=\"app\"]/div/main/div/div[2]/div/div[2]/div/form/div[1]/div[1]/div[1]/div/div[1]/button")).click();
+			
+		}
+		
+		
+
+	}
+
